675e9f9d17e1786e12d1d1cfe8ee2b99209ef179
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2025-02-02 09:42:39 +0700

    Cài Spring Security, tạo đối tượng UserDetails

---
+++ shopapp/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.project.shopapp.configurations;
+
+import com.project.shopapp.models.User;
+import com.project.shopapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    //user's detail object
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return phoneNumber->userRepository
+                .findByPhoneNumber(phoneNumber)
+                .orElseThrow(()->new UsernameNotFoundException("Cannot find user with phone number " + phoneNumber));
+    }
+
+    //mã hóa mật khẩu
+    @Bean
+//    Cách 1: Sử dụng BCryptPasswordEncoder, trong này làm rồi
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+//    Cách 2: Tự thực hiện thuật toán mã hóa
+//    public PasswordEncoder passwordEncoder() {
+//        return new PasswordEncoder() {
+//            @Override
+//            public String encode(CharSequence rawPassword) {
+//                return null;
+//            }
+//
+//            @Override
+//            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+//                return false;
+//            }
+//        };
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
